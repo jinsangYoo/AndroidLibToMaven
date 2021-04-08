40a6cabf4bc7968a814eb1ebe5465b31a353a356
jinsangYoo <milkybboy@gmail.com>
2021-04-08 21:07:27 +0900

    init commit.

---
+++ AndroidLibToMaven/src/main/java/com/github/jinsangYoo/AndroidLibToMaven.java
@@ -0,0 +1,11 @@
+package com.github.jinsangYoo;
+
+import android.annotation.SuppressLint;
+import android.util.Log;
+
+public class AndroidLibToMaven {
+    @SuppressLint("LongLogTag")
+    public static void showToast() {
+        Log.d("AndroidLibToMaven::showToast", "clicked");
+    }
+}
